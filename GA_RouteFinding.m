e83bdcc1eb7c8b18de3b334441ce2ced6a2d0b61
ztallman <123096359+ztallman@users.noreply.github.com>
2023-02-07 17:58:45 -0500

    Add files via upload

--- GA_RouteFinding.m
+++ GA_RouteFinding.m
@@ -19,17 +19,30 @@ clc
 v = 45; %mph
 cap = 20; %carrying capacity
 
-n = 100; %pop size
+n = 200; %pop size
 moves = [0 1;1 0;0 -1;-1 0];
-m = 0.5*n; %# of mutated individuals
-c = 0.5*n; %# of reproducing individuals
+m = round(0.10*n); %# of mutating individuals
+c = round(0.75*n); %# of reproducing individuals
+if rem(c,2) ~= 0
+    c = c + 1;
+end
 L0 = 8*v+2; %max # of stops
 pop(1:n) = struct('path',zeros(L0,2),'turns',zeros(L0,1),'distance',0,'delivered',0);
 
 %Criteria
-chk0 = 99; %# of visited houses - origin
+chk0 = 100; %# of visited houses - origin
 d0 = 100; %distance traveled
 deliver0 = 150; %# of deliveries
+
+figure(1)
+title('Delivery Path')
+
+figure(2)
+xlabel('Generation')
+ylabel('Evaluated Fitness')
+ylim([0 1])
+grid on
+hold on
 
 %%%%%%%%%       Initialization       %%%%%%%%%
 EF = zeros(n,1);
@@ -87,7 +100,7 @@ G = 1;
 p = 1;
 while M_EF <= 0.8
 
-%%%%%%%%% Selection (Roulette Wheel, Elitist(WIP)) %%%%%%%%%
+%%%%%%%%% Selection (Roulette Wheel, Elitist) %%%%%%%%%
     
     TF = sum(EF);
     q = zeros(n,1);
@@ -112,8 +125,8 @@ while M_EF <= 0.8
     %%%%%% Mutation %%%%%%
     ind_m = randi(n,m,1); %indices of mutating individuals
     pop_m = pop0(ind_m);
-    n_m = randi(5); %# of times mutation is applied to an individual
-    for mutations = 1:n_m
+    %n_m = randi([5 round(L0/8)]); %# of times mutation is applied to an individual
+    %for mutations = 1:n_m
         for i = 1:m
             if ind_m ~= ind_max
                 r = randi(L0);
@@ -140,6 +153,36 @@ while M_EF <= 0.8
                     mv_m = pop_m(i).turns(j+1);
                 end
                 pop0(ind_m(i)) = pop_m(i);
+            end
+        end
+    %end
+    
+    %%%%%%%%%    Crossover    %%%%%%%%%
+    ind_c = randi(n,c,1); %indices of reproducing individuals
+    ind_c(ind_c == ind_max) = [];
+    lic = length(ind_c);
+    pop_c = pop0(ind_c);
+    intvl = [round(L0*0.1) round(L0*0.9)]; %bounds for crossover point
+
+    for i = 1:c/2
+        i1 = ind_c(randi(round(lic/2)));
+        pos1 = pop0(i1).path;
+        turn1 = pop(i1).turns;
+        i2 = ind_c(randi([round(lic/2)+1 lic]));
+        pos2 = pop0(i2).path;
+        turn2 = pop0(i2).turns;
+
+        i_crs = find(~(pos1(:,1)-pos2(:,1)) & ~(pos1(:,2)-pos2(:,2)));
+        i_crs = i_crs(i_crs > intvl(1) & i_crs < intvl(2));
+        lc = length(i_crs);
+        if lc ~= 0
+            crosspt = i_crs(randi(lc));
+            for j = crosspt:L0
+                a = pop0(i1).path(j,:);
+                b = pop0(i2).path(j,:);
+
+                pop0(i1).path(j,:) = b;
+                pop0(i2).path(j,:) = a;
             end
         end
     end
@@ -171,7 +214,7 @@ while M_EF <= 0.8
             if chk == 0
                 chk = chk0;
                 a = j;
-                b = b+j;
+                b = b + j;
             end
 
             d = 1/v*sqrt((stops(j-1,1)-x)^2 + (stops(j-1,2)-y)^2);
@@ -183,6 +226,7 @@ while M_EF <= 0.8
         EF(i,1) = 0.2*(total_distance/d0) + 0.8*(delivered/deliver0);
     end
     M_EF = max(EF);
+    EFavg = mean(EF);
     ind_max = find(EF == M_EF);
     lmax = length(ind_max);
     ind_max = ind_max(randi(lmax));
@@ -190,28 +234,32 @@ while M_EF <= 0.8
     x = pop_f.path(:,1); y = pop_f.path(:,2);
     pop = pop0;
 
-    figure (1)
+    figure(1)
     plot(x,y)
-    grid on
-    title('Delivery Path')
     xlim([0 9])
     ylim([0 9])
     lgd = strcat('Generation #',string(G));
     legend(lgd)
+    grid on
 
     %mef(p) = M_EF;
     %g(p) = G;
     figure(2)
     plot(G,M_EF,'g^')
-    xlabel('Generation')
-    ylabel('Evaluated Fitness')
+    plot(G,EFavg,'r*')
     ylim([0 1])
-    grid on
-    hold on
-
+    legend('Max Fitness','Average Fitness')
+    nme = 'final_population';
+    save(nme,'pop_f','G','EF','M_EF');
     G = G+1;
     %p = p+1;
 end
+
+saveas(figure(1),'Path','png');
+saveas(figure(2),'Fitness','png');
+nme = strcat('final_population_G',string(G-1));
+save(nme,'pop_f','G','EF','M_EF');
+
 %% Functions
 
 function k = LM(x,y)
